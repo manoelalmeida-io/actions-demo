f539228687a086165d9f81fe1b69a67869bb572b
Manoel Almeida <manoel.almeida@sptech.school>
2022-10-25 14:13:47 -0300

    Adding demo java spring project

---
+++ src/main/java/school/sptech/actionsdemo/ActionsDemoApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.actionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsDemoApplication.class, args);
+	}
+
+}
